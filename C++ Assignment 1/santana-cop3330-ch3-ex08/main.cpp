f801330b4e3bb5af125cec7cde9e7355befab3cc
dereks5 <90410123+dereks7@users.noreply.github.com>
2021-11-28 01:27:58 +0000

    yuh

---
+++ C++ Assignment 1/santana-cop3330-ch3-ex08/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Derek Santana
+ */
+
+int main() 
+{
+  int num;
+
+  std:: cout << "Input a number\n";
+  std:: cin >> num;
+  
+  if(num%2 == 0)
+  {
+    std:: cout << "The value " << num << " is an even number";
+  }
+  else
+  {
+    std:: cout << "The value " << num << " is an odd number";
+  }
+} 
